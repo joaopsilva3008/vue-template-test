f81253685b9012b513aad068cb759dd64f49ebbb
GARLAND\joao.silva <joao.silva@garland.pt>
2020-08-26 16:26:29 +0100

    first comitt

---
+++ src/views/BookingAir.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <BookingAIR />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BookingAIR from "@/components/BookingAIR.vue";
+
+export default {
+  name: "Home",
+  components: {
+    BookingAIR
+  }
+};
+</script>
