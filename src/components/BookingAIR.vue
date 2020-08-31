b056751796f561fcde63ac25ebffac80670d4301
GARLAND\joao.silva <joao.silva@garland.pt>
2020-08-31 17:57:02 +0100

    New commit

--- src/components/BookingAIR.vue
+++ src/components/BookingAIR.vue
@@ -21,7 +21,50 @@
                     <div id="longtabs">
                         <DxTabs :data-source="longtabs" 
                                 :selected-index.sync="selectedIndex"
+                                @click="switchTabs(longtabs.id)"
                         />
+                    </div>
+                    <div id="tab-client" class="dxTabStyle" v-if="selectedIndex == 0">
+                        <div class="box-header ui-sortable-handle">
+                            <i class="far fa-user bkg-icon-left-menu" style="margin-right:10px"></i>
+                            <h3 class="box-title">{{longtabs[selectedIndex].text}}</h3>
+                        </div>
+                    </div>
+                    <div id="tab-booking" class="dxTabStyle" v-if="selectedIndex == 1">
+                        <div class="box-header ui-sortable-handle">
+                            <i class="far fa-clipboard-list bkg-icon-left-menu" style="margin-right:10px"></i>
+                            <h3 class="box-title">{{longtabs[selectedIndex].text}}</h3>
+                        </div>
+                    </div>
+                    <div id="tab-origin-destination" class="dxTabStyle" v-if="selectedIndex == 2">
+                        <div class="box-header ui-sortable-handle">
+                            <i class="far fa-map-marker-alt bkg-icon-left-menu" style="margin-right:10px"></i>
+                            <h3 class="box-title">{{longtabs[selectedIndex].text}}</h3>
+                        </div>
+                    </div>
+                    <div id="tab-booking-detail" class="dxTabStyle" v-if="selectedIndex == 3">
+                        <div class="box-header ui-sortable-handle">
+                            <i class="far fa-pallet-alt bkg-icon-left-menu" style="margin-right:10px"></i>
+                            <h3 class="box-title">{{longtabs[selectedIndex].text}}</h3>
+                        </div>
+                    </div>
+                    <div id="tab-shipments" class="dxTabStyle" v-if="selectedIndex == 4">
+                        <div class="box-header ui-sortable-handle">
+                            <i class="far fa-conveyor-belt-alt bkg-icon-left-menu" style="margin-right:10px"></i>
+                            <h3 class="box-title">{{longtabs[selectedIndex].text}}</h3>
+                        </div>
+                    </div>
+                    <div id="tab-other-info" class="dxTabStyle" v-if="selectedIndex == 5">
+                        <div class="box-header ui-sortable-handle">
+                            <i class="far fa-file-invoice bkg-icon-left-menu" style="margin-right:10px"></i>
+                            <h3 class="box-title">{{longtabs[selectedIndex].text}}</h3>
+                        </div>
+                    </div>
+                    <div id="tab-revenue-costs" class="dxTabStyle" v-if="selectedIndex == 6">
+                        <div class="box-header ui-sortable-handle">
+                            <i class="far fa-coins bkg-icon-left-menu" style="margin-right:10px"></i>
+                            <h3 class="box-title">{{longtabs[selectedIndex].text}}</h3>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -40,53 +83,29 @@
     },
     data() {
         return {
-        selectedIndex: 0,
-        longtabs
+            selectedIndex: 0,
+            longtabs
         };
     },
     methods: {
         saveBooking() {
             alert('Hello world!')
+        },
+        switchTabs(id){
+            console.log(id)
         }
     },
     name: "BookingAIR"
     };
 
     export const longtabs = [
-    { text: 'Client' },
-    { text: 'Booking' },
-    { text: 'Origin and Destination' },
-    { text: 'Booking Detail' },
-    { text: 'Shipments' },
-    { text: 'Other Info' }
+        { text: 'Client',id: 1 },
+        { text: 'Booking',id: 2 },
+        { text: 'Origin and Destination',id: 3 },
+        { text: 'Booking Detail',id: 4 },
+        { text: 'Shipments',id: 5 },
+        { text: 'Other Info',id: 6 },
+        { text: 'Revenue and Costs',id: 7 }
     ];
 
 </script>
-<style>
-    .content{
-        text-align: justify;
-        margin-top: 25px;
-    }
-
-    #longtabs{
-        margin-top: 20px;
-    }
-
-    #scrolledtabs {
-        margin-top: 20px;
-    }
-
-    #tabs {
-        margin-top: 60px;
-    }
-
-    .caption{
-        font-size: 16px;
-        padding-bottom: 3px;
-        padding-left: 10px;
-    }
-
-    .left-aligned {
-        text-align: left;
-    }
-</style>
